5c141a86005de8b080ae329c57abf94338b24c15
Mushrooms <fernandobermudez1606@gmail.com>
2024-11-08 17:01:33 -0600

    Se ha empezado la conexion con la DB en Pg solicitudes, aun no funciona. HAY UNA NUEVA DB. FAVOR IMPORTARLA

---
+++ MenuPrincipal/BD/Models/IngresoPrestamo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPrincipal.BD.Models
+{
+    public class IngresoPrestamo
+    {
+        public int UsuarioId { get; set; }
+        public int LibroId { get; set;}
+        public DateTime FechaSolicitud { get; set; }
+        public string EstadoSolicitud { get; set; }
+        public string TiempoEspera { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+        public DateTime FechaDevolucion { get; set; }
+        public string EstadoPrestamo { get; set; }
+        public string TipoPrestamo { get; set; }
+        public string TiempoEntrega { get; set; }
+        public int Renovaciones {  get; set; }
+        public DateTime FechaRenovacion { get; set; }
+    }
+}
